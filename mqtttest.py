65a6bb7e469dc223acdc15d7a416d52e87bf65b1
peter-nisbet <peternisbet265@live.ca>
2018-07-18 22:36:35 -0400

    Added Json Parse for Door

--- mqtttest.py
+++ mqtttest.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import ssl
+import json
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -13,7 +14,8 @@ def on_connect(client, userdata, flags, rc):
 # This callback is for the mailbox door control topic
 def doorcontrol_callback(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    if(str(msg.payload) == "Open"):
+    doorStatus=json.loads(str(msg.payload))
+    if(doorStatus['status'] == "Open"):
 	print("Door is now Open")
     else:
 	print("Door is now Closed")
@@ -25,11 +27,18 @@ client.on_connect = on_connect
 client.message_callback_add("mailbox/doorcontrol", doorcontrol_callback)
 
 #Set up TLS secure socket to communicate with the broker
+client.tls_set(ca_certs='/home/peter/Desktop/ca-certs/ca.crt',
+	certfile='/home/peter/Desktop/certs/client.crt', 
+	keyfile='/home/peter/Desktop/certs/client.key',
+	cert_reqs=ssl.CERT_NONE,	
+	tls_version=ssl.PROTOCOL_TLSv1_2)
+'''
 client.tls_set(ca_certs='/home/linaro/ca-certs/ca.crt',
 	certfile='/home/linaro/certs/client.crt', 
 	keyfile='/home/linaro/certs/client.key',
 	cert_reqs=ssl.CERT_NONE,	
 	tls_version=ssl.PROTOCOL_TLSv1_2)
+'''
 
 #Added due to mismatched common name in certificates
 client.tls_insecure_set(True)
